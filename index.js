9d587ca686c2e61b1b3e27df8cd28d305cf8fe07
silva <htsilvakt04@gmail.com>
2018-03-13 16:42:30 +0700

    finish testing reducer function, at the minute of: 8'

--- index.js
+++ index.js
@@ -1,37 +1,89 @@
-function createStore () {
-    // the state tree
-    // get
-    // listen
-    // update
+let addAction = {
+    type: 'ADD_TODO',
+    todo: {
+        id: 1,
+        name: 'Learn more than Redux',
+        complete: false
+    }
+};
 
+let removeAction = {
+    type: 'REMOVE_TODO',
+    id: 2
+};
+
+let toggleAction = {
+    type: 'TOGGLE_TODO',
+    id: 3
+};
+
+// be responsible for returning the "Next state" of our app after a particular event happened
+function reducer (currentState = [], action) {
+    if (action.type === 'ADD_TODO') {
+        return currentState.concat([action.todo]);
+    }
+    // switch (action.type) {
+    //     case 'ADD_TODO':
+    //         return currentState.concat([action.todo]);
+    //     case 'REMOVE_TODO':
+    //         // remove todo
+    //     default:
+    //         return currentState;
+    // }
+    return currentState;
+}
+
+
+function createStore () {
     let state;
     let listeners = [];
 
     const getState = () => state;
+
     const subscribe = (listenerCallback) => {
         // push the callback to listeners array (tract how many subscribe functions has being triggered)
-        const length = listeners.push(listenerCallback);
+        listeners.push(listenerCallback);
 
         return () => {
-            listeners = listeners.filter((item) => {
+            listeners = listeners.filter( item => {
                 return item !== listenerCallback;
             })
         };
-
-        // check if the state was changed, compare the last and the current one
-        // need to have a history variable in order to do that
-
-        // if change then return the change
-
-        // else return not thing
     };
 
+    let dispatch = (action) => {
+        // call reducer
+        state = reducer(state, action);
+        // invoke listener function
+        listeners.forEach(listener => listener(state));
+        // return new State || null
 
+    };
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 }
+
+// new file
+let store = createStore();
+
+let subscriber = store.subscribe((newState) => {
+    console.log('new state is ' + JSON.stringify( newState));
+});
+// get this from database or from user input form
+
+
+let result = store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 1,
+        name: 'Learn more than Redux',
+        complete: false
+    }
+});
+
 
 
 
